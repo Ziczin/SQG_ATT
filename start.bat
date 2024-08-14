96864005053217d6884f1ac29fc46b200028be83
Ziczin <ziczin121@gmail.com>
2024-08-14 16:52:26 +0400

    Created a quick launch files

---
+++ start.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set "flag1="
+set "flag2="
+
+if not exist "config.cfg" (
+    set "genconf=--genconf"
+)
+
+if not exist "text.txt" (
+    set "gentext=--gentext"
+)
+
+python src/main.py config.cfg text.txt %genconf% %gentext%
+pause
